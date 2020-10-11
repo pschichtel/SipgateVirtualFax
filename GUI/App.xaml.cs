aa4111e38aa99b7924f4fd0d0f7cc7136e40e713
Phillip Schichtel <phillip@schich.tel>
2020-10-12 00:47:10 +0200

    prevent multiple instances, instead bring the existing instance to the foreground

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
 
 namespace SipGateVirtualFaxGui
 {
@@ -7,13 +11,39 @@ namespace SipGateVirtualFaxGui
     /// </summary>
     public partial class App
     {
-        protected override void OnStartup(StartupEventArgs e)
+        private readonly Mutex _mutex;
+
+
+        public App()
         {
+            // Try to grab mutex
+            _mutex = new Mutex(true, "tel-schich-sipgate-virtual-fax", out var createdNew);
+
+            if (!createdNew)
+            {
+                // Bring other instance to front and exit.
+                Process current = Process.GetCurrentProcess();
+                foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+                {
+                    if (process.Id != current.Id)
+                    {
+                        WindowHelper.BringWindowToForeground(process);
+                        break;
+                    }
+                }
+
+                Shutdown();
+            }
+            else
+            {
+                Exit += HandleExit;
+            }
         }
 
-        protected override void OnExit(ExitEventArgs e)
+        protected virtual void HandleExit(object sender, EventArgs e)
         {
             FaxStuff.Instance.FaxScheduler.Dispose();
+            _mutex.Close();
         }
     }
 }
