251483592386a2ba71129450be06763f50599e0d
Phillip Schichtel <phillip@schich.tel>
2022-01-30 15:34:02 +0100

    generic implementation of implicit flow

--- Core/Core/Sipgate/SipgateFaxClient.cs
+++ Core/Core/Sipgate/SipgateFaxClient.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Newtonsoft.Json;
 using NLog;
 
@@ -36,13 +37,104 @@ namespace SipgateVirtualFax.Core.Sipgate
         }
     }
 
+    public interface IOAuthImplicitFlowHandler
+    {
+        /// <summary>
+        /// Pull existing access_token out of a hat
+        /// </summary>
+        /// <returns>access_token or null</returns>
+        Task<string?> GetAccessTokenFromStorage();
+        
+        /// <summary>
+        /// Take the authorizationUri, present it to the user in some way, get it redirected to the redirect_uri
+        /// and return the redirection target.
+        /// </summary>
+        /// <param name="authorizationUri">complete authorization URI</param>
+        /// <returns>the redirection target</returns>
+        Task<Uri> Authorize(Uri authorizationUri);
+    }
+
+    public class OAuth2ImplicitFlowException : Exception
+    {
+        public OAuth2ImplicitFlowException(string message) : base(message)
+        {
+        }
+    }
+
     public class OAuth2ImplicitFlowHeaderProvider : IAuthorizationHeaderProvider
     {
+        private IOAuthImplicitFlowHandler _handler;
+        private readonly Uri _authorizationUri;
+        private readonly Uri _redirectUri;
+        private readonly string _clientId;
+        private readonly string _scope;
+
+        public OAuth2ImplicitFlowHeaderProvider(IOAuthImplicitFlowHandler handler, Uri authorizationUri, Uri redirectUri, string clientId, string[] scopes)
+        {
+            _handler = handler;
+            _authorizationUri = authorizationUri;
+            _redirectUri = redirectUri;
+            _clientId = clientId;
+            _scope = string.Join(" ", scopes.Prepend("openid"));;
+        }
+
         public bool RetryOn401 => true;
 
-        public Task<string> GetHeaderValue(bool retry)
+        private string FormatAsBearer(string accessToken) => $"Bearer {accessToken}";
+
+        public async Task<string> GetHeaderValue(bool retry)
         {
-            throw new NotImplementedException();
+            var existingToken = await _handler.GetAccessTokenFromStorage();
+            if (existingToken != null)
+            {
+                return FormatAsBearer(existingToken);
+            }
+
+            var state = Guid.NewGuid().ToString();
+            var nonce = Guid.NewGuid().ToString();
+            var authorizationUriParams = HttpUtility.ParseQueryString(_authorizationUri.Query);
+            authorizationUriParams.Add("response_type", "id_token token");
+            authorizationUriParams.Add("client_id", _clientId);
+            authorizationUriParams.Add("redirect_uri", _redirectUri.ToString());
+            authorizationUriParams.Add("scope", _scope);
+            authorizationUriParams.Add("state", state);
+            authorizationUriParams.Add("nonce", nonce);
+            
+            var finalAuthUri = new UriBuilder(_authorizationUri)
+            {
+                Query = authorizationUriParams.ToString()
+            }.Uri;
+            var redirectionTarget = await _handler.Authorize(finalAuthUri);
+            
+            var fragment = redirectionTarget.Fragment;
+            if (fragment == null)
+            {
+                throw new OAuth2ImplicitFlowException("Redirect URI did not contain a fragment!");
+            }
+
+            var redirectParams = HttpUtility.ParseQueryString(fragment);
+            var returnedState = redirectParams.Get("state");
+            if (returnedState != state)
+            {
+                throw new OAuth2ImplicitFlowException("Returned state did not match!");
+            }
+
+            var error = redirectParams.Get("error");
+            if (!string.IsNullOrEmpty(error))
+            {
+                var description = redirectParams.Get("error_description");
+                var uri = redirectParams.Get("error_uri");
+                throw new OAuth2ImplicitFlowException(
+                    $"authorization error: code={error}, description={description}, uri={uri}");
+            }
+
+            var accessToken = redirectParams.Get("access_token");
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                throw new OAuth2ImplicitFlowException("No error, but also not access_token received!");
+            }
+                
+            return FormatAsBearer(accessToken);
         }
     }
     
